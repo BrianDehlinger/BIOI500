50f0811bf34d9ebcb4477cee87f3edb228ffa80b
Dehlinger <bdehlinger@luc.edu>
2020-04-27 19:03:24 -0500

    FINISHED

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import os
 
 # I know that using os.system is sloppy. It was extremely fast to write. Ideally, this can be made to use subprocess.Popen. However, this is in fact more readable.
-os.system("sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian$os.system("sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9")
+os.system("sudo wget -O- http://neuro.debian.net/lists/xenial.us-ca.full | sudo tee /etc/apt/sources.list.d/neurodebian")
+os.system("sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9")
 os.system("sudo apt-get update")
 os.system("sudo apt-get install -y singularity-container")
 os.system("source ~/.bashrc")
